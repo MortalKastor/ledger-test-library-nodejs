7cdd55dd9ce6819c81ac99c1dc99c592d4b5de8c
KhalilBellakrid <khalil.bellakrid@ledger.fr>
2018-03-01 11:23:00 +0100

    clean NodeJsMarshal, inherits now from CppMarshal

--- deps/djinni/src/source/NodeJsMarshal.scala
+++ deps/djinni/src/source/NodeJsMarshal.scala
@@ -7,27 +7,90 @@ import djinni.writer.IndentWriter
 
 import scala.collection.mutable.ListBuffer
 
-//noinspection RedundantBlock,RedundantBlock
-class NodeJsMarshal(spec: Spec) extends Marshal(spec) {
+class NodeJsMarshal(spec: Spec) extends CppMarshal(spec) {
 
-  protected val cppMarshal = new CppMarshal(spec)
+  override def typename(tm: MExpr): String = toNodeType(tm, None, Seq())
+  override def fqTypename(tm: MExpr): String = toNodeType(tm, Some(spec.cppNamespace), Seq())
+  override def typename(name: String, ty: TypeDef): String = ty match {
+    case e: Enum => idNode.enumType(name)
+    case i: Interface => idNode.ty(name)
+    case r: Record => idNode.ty(name)
+  }
 
   override def paramType(tm: MExpr): String = toNodeParamType(tm)
-
   override def fqParamType(tm: MExpr): String = toNodeParamType(tm, Some(spec.cppNamespace))
-
   private def toNodeParamType(tm: MExpr, namespace: Option[String] = None, scopeSymbols: Seq[String] = Seq()): String = {
     toNodeType(tm, namespace, scopeSymbols)
   }
 
-  def returnType(ret: Option[TypeRef], scopeSymbols: Seq[String]): String = {
-    ret.fold("void")(toNodeType(_, None, scopeSymbols))
-  }
-
+  override def returnType(ret: Option[TypeRef], scopeSymbols: Seq[String]): String = { ret.fold("void")(toNodeType(_, None, scopeSymbols)) }
   override def returnType(ret: Option[TypeRef]): String = ret.fold("void")(toNodeType(_, None))
-
   override def fqReturnType(ret: Option[TypeRef]): String = {
     ret.fold("void")(toNodeType(_, Some(spec.cppNamespace)))
+  }
+
+  def hppReferences(m: Meta, exclude: String, forwardDeclareOnly: Boolean, nodeMode: Boolean): Seq[SymbolReference] = m match {
+    case d: MDef => d.body match {
+      case i: Interface =>
+        val base = if (d.name != exclude) {
+
+          var cppInterfaceImport = s""""${idNode.ty(d.name)}"""
+          if (i.ext.cpp) {
+            cppInterfaceImport = s"${cppInterfaceImport}Cpp"
+          }
+
+          cppInterfaceImport = s"""$cppInterfaceImport.${spec.cppHeaderExt}""""
+          val nodeInterfaceImport = s""""${spec.nodeIncludeCpp}/${d.name}.${spec.cppHeaderExt}""""
+
+          if (nodeMode) {
+            List(ImportRef("<memory>"), ImportRef(cppInterfaceImport), ImportRef(nodeInterfaceImport))
+          } else {
+            List(ImportRef("<memory>"), ImportRef(cppInterfaceImport))
+          }
+
+        } else
+          List(ImportRef("<memory>"))
+
+        spec.cppNnHeader match {
+          case Some(nnHdr) => ImportRef(nnHdr) :: base
+          case _ => base
+        }
+      case _ => super.hppReferences(m, exclude, forwardDeclareOnly)
+    }
+    case _ => super.hppReferences(m, exclude, forwardDeclareOnly)
+  }
+
+  override def cppReferences(m: Meta, exclude: String, forwardDeclareOnly: Boolean): Seq[SymbolReference] = {
+
+    if (!forwardDeclareOnly) {
+      List()
+    } else {
+      m match {
+        case d: MDef =>
+          val nodeRecordImport = s"${spec.nodeIncludeCpp}/${d.name}"
+          d.body match {
+            case r: Record =>
+              if (d.name != exclude) {
+                List(ImportRef(include(nodeRecordImport, r.ext.cpp)))
+              } else {
+                List()
+              }
+            case e: Enum =>
+              if (d.name != exclude) {
+                List(ImportRef(include(nodeRecordImport)))
+              } else {
+                List()
+              }
+            case _ => List()
+          }
+        case _ => List()
+      }
+    }
+  }
+
+  override def include(ident: String, isExtendedRecord: Boolean = false): String = {
+    val prefix = if (isExtendedRecord) spec.cppExtendedRecordIncludePrefix else spec.cppIncludePrefix
+    q(prefix + spec.cppFileIdentStyle(ident) + "." + spec.cppHeaderExt)
   }
 
   private def toNodeType(ty: TypeRef, namespace: Option[String] = None, scopeSymbols: Seq[String] = Seq()): String =
@@ -37,24 +100,20 @@ class NodeJsMarshal(spec: Spec) extends Marshal(spec) {
 
     def base(m: Meta): String = m match {
       case p: MPrimitive => p.nodeJSName
-      case MString => if (spec.cppUseWideStrings) "std::wstring" else "String"
+      case MString => "String"
       case MDate => "Date"
       case MBinary => "Object"
-      case MOptional => spec.cppOptionalTemplate
+      case MOptional => "MaybeLocal"
       case MList => "Array"
       case MSet => "Set"
       case MMap => "Map"
       case d: MDef =>
         d.defType match {
-          case DEnum => withNamespace(idCpp.enumType(d.name), namespace, scopeSymbols)
-          case DRecord => withNamespace(idCpp.ty(d.name), namespace, scopeSymbols)
           case DInterface => withNamespace(idNode.ty(d.name), namespace, scopeSymbols)
+          case _ => super.toCppType(tm, namespace, scopeSymbols)
         }
-      case e: MExtern => e.defType match {
-        case DInterface => s"std::shared_ptr<${e.cpp.typename}>"
-        case _ => e.cpp.typename
-      }
       case p: MParam => idNode.typeParam(p.name)
+      case _ => super.toCppType(tm, namespace, scopeSymbols)
     }
 
     def expr(tm: MExpr): String = {
@@ -102,25 +161,20 @@ class NodeJsMarshal(spec: Spec) extends Marshal(spec) {
     withNs(ns, name)
   }
 
-  override def fieldType(tm: MExpr): String = typename(tm)
-
-  override def typename(tm: MExpr): String = toNodeType(tm, None, Seq())
-
-  override def fqFieldType(tm: MExpr): String = fqTypename(tm)
-
-  override def fqTypename(tm: MExpr): String = toNodeType(tm, Some(spec.cppNamespace), Seq())
+  override def toCpp(tm: MExpr, expr: String): String = throw new AssertionError("cpp to cpp conversion")
+  override def fromCpp(tm: MExpr, expr: String): String = throw new AssertionError("cpp to cpp conversion")
 
   def toCppArgument(tm: MExpr, converted: String, converting: String, wr: IndentWriter, namespace: Option[String] = None, scopeSymbols: Seq[String] = Seq()): IndentWriter = {
 
     def toCppContainer(container: String): IndentWriter = {
 
       if (!tm.args.isEmpty) {
-        val cppTemplType = cppMarshal.paramType(tm.args(0), true)
+        val cppTemplType = super.paramType(tm.args(0), true)
         val nodeTemplType = paramType(tm.args(0))
 
         if (container == "Map" && tm.args.length > 1) {
 
-          val cppTemplValueType = cppMarshal.paramType(tm.args(1), true)
+          val cppTemplValueType = super.paramType(tm.args(1), true)
           val nodeTemplValueType = paramType(tm.args(1))
 
           wr.wl(s"map<$cppTemplType, $cppTemplValueType> $converted;")
@@ -152,7 +206,7 @@ class NodeJsMarshal(spec: Spec) extends Marshal(spec) {
 
     }
 
-    val cppType = cppMarshal.paramType(tm, needRef = true)
+    val cppType = super.paramType(tm, needRef = true)
 
     def base(m: Meta): IndentWriter = m match {
       case p: MPrimitive => wr.wl(s"auto $converted = Nan::To<${toSupportedCppNativeTypes(p.cName)}>($converting).FromJust();")
@@ -187,7 +241,7 @@ class NodeJsMarshal(spec: Spec) extends Marshal(spec) {
           case i: Interface =>
 
             val nodeType = paramType(tm)
-            val cppType = cppMarshal.paramType(tm, needRef = true)
+            val cppType = super.paramType(tm, needRef = true)
             wr.wl(s"Local<Object> njs_arg_$converted = $converting->ToObject(context).ToLocalChecked();")
             wr.wl
             wr.wl(s"$nodeType *njs_obj_$converted = static_cast<$nodeType *>(Nan::GetInternalFieldPointer(njs_arg_$converted,0));")
@@ -264,7 +318,7 @@ class NodeJsMarshal(spec: Spec) extends Marshal(spec) {
             wr.wl
           case i: Interface =>
             val nodeType = paramType(tm)
-            val cppType = cppMarshal.paramType(tm, needRef = true)
+            val cppType = super.paramType(tm, needRef = true)
             //Use wrap methods
             wr.wl(s"auto $converted = ${idNode.ty(d.name)}::wrap($converting);")
             wr.wl
@@ -277,111 +331,6 @@ class NodeJsMarshal(spec: Spec) extends Marshal(spec) {
     }
 
     base(tm.base)
-  }
-
-  def hppReferences(m: Meta, exclude: String, forwardDeclareOnly: Boolean, nodeMode: Boolean): Seq[SymbolReference] = m match {
-    case p: MPrimitive => p.idlName match {
-      case "i8" | "i16" | "i32" | "i64" => List(ImportRef("<cstdint>"))
-      case _ => List()
-    }
-    case MString => List(ImportRef("<string>"))
-    case MDate => List(ImportRef("<chrono>"))
-    case MBinary => List(ImportRef("<vector>"), ImportRef("<cstdint>"))
-    case MOptional => List(ImportRef(spec.cppOptionalHeader))
-    case MList => List(ImportRef("<vector>"))
-    case MSet => List(ImportRef("<unordered_set>"))
-    case MMap => List(ImportRef("<unordered_map>"))
-    case d: MDef => d.body match {
-      case r: Record =>
-        if (d.name != exclude) {
-          if (forwardDeclareOnly) {
-            List(DeclRef(s"struct ${typename(d.name, d.body)};", Some(spec.cppNamespace)))
-          } else {
-            List(ImportRef(include(d.name, r.ext.cpp)))
-          }
-        } else {
-          List()
-        }
-      case e: Enum =>
-        if (d.name != exclude) {
-          if (forwardDeclareOnly) {
-            List(DeclRef(s"enum class ${typename(d.name, d.body)};", Some(spec.cppNamespace)))
-          } else {
-            List(ImportRef(include(d.name)))
-          }
-        } else {
-          List()
-        }
-      case i: Interface =>
-        val base = if (d.name != exclude) {
-
-          var cppInterfaceImport = s""""${idNode.ty(d.name)}"""
-          if (i.ext.cpp) {
-            cppInterfaceImport = s"${cppInterfaceImport}Cpp"
-          }
-
-          cppInterfaceImport = s"""$cppInterfaceImport.${spec.cppHeaderExt}""""
-          val nodeInterfaceImport = s""""${spec.nodeIncludeCpp}/${d.name}.${spec.cppHeaderExt}""""
-
-          if (nodeMode) {
-            List(ImportRef("<memory>"), ImportRef(cppInterfaceImport), ImportRef(nodeInterfaceImport))
-          } else {
-            List(ImportRef("<memory>"), ImportRef(cppInterfaceImport))
-          }
-
-        } else
-          List(ImportRef("<memory>"))
-
-        spec.cppNnHeader match {
-          case Some(nnHdr) => ImportRef(nnHdr) :: base
-          case _ => base
-        }
-    }
-
-    case e: MExtern => e.defType match {
-      case DInterface => List(ImportRef(e.cpp.header))
-      case _ => List(ImportRef(e.cpp.header))
-    }
-    case p: MParam => List()
-  }
-
-  def typename(name: String, ty: TypeDef): String = ty match {
-    case e: Enum => idNode.enumType(name)
-    case i: Interface => idNode.ty(name)
-    case r: Record => idNode.ty(name)
-  }
-
-  def include(ident: String, isExtendedRecord: Boolean = false): String = {
-    val prefix = if (isExtendedRecord) spec.cppExtendedRecordIncludePrefix else spec.cppIncludePrefix
-    q(prefix + spec.cppFileIdentStyle(ident) + "." + spec.cppHeaderExt)
-  }
-
-  def cppReferences(m: Meta, exclude: String, forwardDeclareOnly: Boolean): Seq[SymbolReference] = {
-
-    if (!forwardDeclareOnly) {
-      List()
-    } else {
-      m match {
-        case d: MDef =>
-          val nodeRecordImport = s"${spec.nodeIncludeCpp}/${d.name}"
-          d.body match {
-            case r: Record =>
-              if (d.name != exclude) {
-                List(ImportRef(include(nodeRecordImport, r.ext.cpp)))
-              } else {
-                List()
-              }
-            case e: Enum =>
-              if (d.name != exclude) {
-                List(ImportRef(include(nodeRecordImport)))
-              } else {
-                List()
-              }
-            case _ => List()
-          }
-        case _ => List()
-      }
-    }
   }
 
   private def toSupportedCppNativeTypes(inputType: String): String = {
